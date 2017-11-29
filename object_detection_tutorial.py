9365a2d7710f974086fffb98a79b4731bd87ca8b
Chad Hart <chadwallacehart>
2017-11-28 20:33:33 -0500

    object detection tutorial

---
+++ object_detection_tutorial.py
@@ -0,0 +1,142 @@
+# IMPORTS
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+# from matplotlib import pyplot as plt ### CWH
+from PIL import Image
+
+if tf.__version__ != '1.4.0':
+  raise ImportError('Please upgrade your tensorflow installation to v1.4.0!')
+
+# ENV SETUP  ### CWH: remove matplot display and manually add paths to references
+'''
+# This is needed to display the images.
+%matplotlib inline
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+'''
+
+# Object detection imports
+
+from object_detection.utils import label_map_util    ### CWH: Add object_detection path
+
+#from object_detection.utils import visualization_utils as vis_util ### CWH: used for visualization
+
+# Model Preparation
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('object_detection/data', 'mscoco_label_map.pbtxt') ### CWH: Add object_detection path
+
+NUM_CLASSES = 90
+
+
+# Download Model
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
+
+
+# Load a (frozen) Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+# Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+# Helper code
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+# Detection
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+PATH_TO_TEST_IMAGES_DIR = 'object_detection/test_images' #cwh
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    for image_path in TEST_IMAGE_PATHS:
+      image = Image.open(image_path)
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      image_np = load_image_into_numpy_array(image)
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      # Actual detection.
+      (boxes, scores, classes, num) = sess.run(
+          [detection_boxes, detection_scores, detection_classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+
+      ### CWH: below is used for visualizing with Matplot
+      '''
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      plt.figure(figsize=IMAGE_SIZE)
+      plt.imshow(image_np)  
+      '''
+
+      ### CWH: Print the object details to the console instead of visualizing them with the code above
+
+      classes = np.squeeze(classes).astype(np.int32)
+      scores = np.squeeze(scores)
+      boxes = np.squeeze(boxes)
+
+      threshold = 0.50  #CWH: set a minimum score threshold of 50%
+
+      print("detected %s objects in %s above a %s score" % ( sum(n>threshold for n in scores), image_path, threshold))
+      for c in range(0, len(classes)):
+        if scores[c] > threshold:
+            class_name = category_index[classes[c]]['name']
+            print(" object %s is a %s - score: %s, location: %s" % (c, class_name, scores[c], boxes[c]))
---
+++ setup.py
@@ -0,0 +1,57 @@
+'''Setup script for object_detection with webrtc'''
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRED_PACKAGES = ['Pillow>=1.0', 'Flask', 'tensorflow', 'six', 'matplotlib']
+
+setup(
+    name='webrtc_object_detection',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
+    packages=[p for p in find_packages()],
+    description='Tensorflow Object Detection with WebRTC',
+)
+
+'''Download the Object Dectection directory'''
+import six.moves.urllib as urllib
+from zipfile import ZipFile
+import os
+import re
+import shutil
+
+print("\n\nDownloading the TensorFlow API from Github...")
+
+REPOSITORY_ZIP_URL = 'https://github.com/tensorflow/models/archive/master.zip'
+
+try:
+    filename, headers = urllib.request.urlretrieve(REPOSITORY_ZIP_URL)
+    #filename = 'models-master.zip'
+
+    target_path = os.path.join(os.getcwd(), 'object_detection/')
+    temp_path = filename + "_dir"
+
+    with ZipFile(filename, 'r') as zip_file:
+        files = zip_file.namelist()
+        files_to_extract = [f for f in files if f.startswith(('models-master/research/object_detection/'))]
+        zip_file.extractall(temp_path, files_to_extract)
+        print("Copying TensorFlow Object API files to %s" % target_path)
+        shutil.move(os.path.join(temp_path, 'models-master/research/object_detection/'), target_path)
+        os.removedirs(os.path.join(temp_path, 'models-master/research/'))
+
+except:
+    print("Problem downloading the TensorFlow Object API. \n"
+          "Try running `git clone https://github.com/tensorflow/models.git`.\n"
+          "Then `cp /research/object_detection to /object_detection` instead")
+
+
+'''Compile Protobufs'''
+import subprocess
+
+try:
+    subprocess.Popen('protoc object_detection/protos/*.proto --python_out=.')
+
+except:
+    print("Error compiling Protocolbufs")
